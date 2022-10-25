5bf255a05062189a56a07c9bfa91ad2225e5842f
Ros <72873067+paularosm@users.noreply.github.com>
2022-10-25 20:34:11 +0200

    final

--- experiment.R
+++ experiment.R
@@ -1,12 +1,13 @@
-#hello world
+# Saving the data in the dataset
 class2022 <- read.csv("data.csv", sep=",")
 class2022
-View(class2022)
 
+#Deleting the first row
 class2022 <- class2022[-1, ]
 
 View(class2022)
 
+# Mean of extroversion by profile
 programmers <- class2022[class2022$Profile == "Programmer", ]
 Designers <- class2022[class2022$Profile == "Designer", ]
 Artists <- class2022[class2022$Profile == "Artist", ]
@@ -15,11 +16,18 @@ meanProgExtrov <- mean(programmers$Extroversion)
 meanDesignExtrov <- mean(Designers$Extroversion)
 meanArtExtrov <- mean(Artists$Extroversion)
 
+# Creation of barplot
 barplot(c(meanProgExtrov, meanDesignExtrov, meanArtExtrov), main = "Mean of extroversion", ylab = "% of extroversion", names.arg = c("Programmers", "Designers", "Artists"), col = c("pink", "orange", "brown"))
 
+# Mean of extroversion by genre
 mens <- class2022[class2022$Sex..F.M..... == "M", ]
-woman  <- class2022[class2022$Sex..F.M..... == "F", ]
+women  <- class2022[class2022$Sex..F.M..... == "F", ]
 
-mean(mens$Extroversion)
-mean(woman$Extroversion)
+meanMen <- mean(mens$Extroversion)
+meanWomen <-mean(women$Extroversion)
 
+# Creation of pie chart 
+lblsMenWomen <- c("Men", "Women")
+lblsMenWomen <- paste(lblsMenWomen, c(round(meanMen, 2), round(meanWomen, 2)))
+
+pie(c(meanMen, meanWomen), labels = lblsMenWomen, main = "Mean of extroversion by genre")
