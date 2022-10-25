23ee472b009346cb7d173553338a89df7777e104
edu <eduard.corral@accenture.com>
2022-10-25 17:08:48 +0200

    comparasion of extroversion

--- experiment.R
+++ experiment.R
@@ -7,4 +7,10 @@ class2022 <- class2022[-1, ]
 
 View(class2022)
 
+programmers <- class2022[class2022$Profile == "Programmer", ]
+Designers <- class2022[class2022$Profile == "Designer", ]
+Artists <- class2022[class2022$Profile == "Artist", ]
 
+mean(programmers$Extroversion)
+mean(Designers$Extroversion)
+mean(Artists$Extroversion)
