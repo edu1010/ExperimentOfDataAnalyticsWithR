c1ec31dc51aa214c0e2ccf9874dd0b1212ef43a4
edu <eduard.corral@accenture.com>
2022-11-15 18:19:28 +0100

    ejemplo merge

--- Projecte/infoPractica.R
+++ Projecte/infoPractica.R
@@ -2,3 +2,22 @@
 #RMarkdown
 #intall lybrary -> tools ->installs package-> RMarkdown
 
+#Exemple lybrary dplyr
+library(dplyr)
+
+userid<-c("AA","BB","AA","CX","AA","AB")
+minutes<-c(10,4,20,24,18,2)
+ds<-data.frame(userid,minutes)
+
+#cuanto tiempo esta cada usuario + de media
+summarise(group_by(ds,userid),
+          ntimes=length(minutes),
+          avmins=mean(minutes))
+
+#exple merge
+ds2<-data.frame(id=c("AA","AB","BB","CX"),
+                names=c("Mikel","Pau","Marcelino","Jardines"))
+
+merge(ds,ds2,by.x="userid",by.y="id")
+
+
